e8177ff2f9b8400e2d2e80798d3ea2735dd66f38
quanghoaluu1 <hoalqse183138@fpt.edu.vn>
2025-01-14 16:08:54 +0700

    Homepage

---
+++ src/components/CategorySlider/CategorySlide.tsx
@@ -0,0 +1,47 @@
+﻿import React from 'react';
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation} from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/navigation';
+const categories = [
+    {name: "Sữa Rửa Mặt", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_1.jpg?v=276"},
+    {name: "Tẩy Trang", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_2.jpg?v=276"},
+    {name: "Chống Nắng", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_3.jpg?v=276"},
+    {name: "Chăm Sóc Da Dầu Mụn", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_4.jpg?v=276"},
+    {name: "Trắng Da - Thâm Nám", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_5.jpg?v=276"},
+    {name: "Dưỡng ẩm", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_6.jpg?v=276"},
+    {name: "Dầu gội", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_7.jpg?v=276"},
+    {name: "Thiết bị làm đẹp ", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_8.jpg?v=276"},
+    {name: "Tẩy tế bào chết", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_9.jpg?v=276"},
+    {name: "Dưỡng trắng", url: "//theme.hstatic.net/1000097940/1000899682/14/coll_10.jpg?v=276"}
+]
+const CategorySlide = () => {
+    return (
+        <div className={"w-full max-w-4xl mx-auto"}>
+            <h2 className="text-2xl font-bold text-center mb-6">DANH MỤC NỔI BẬT</h2>
+            <Swiper
+                modules={[Navigation]}
+                navigation
+                spaceBetween={25}
+                slidesPerView={2}
+                className={"category-slider"}
+            >
+                {categories.map((category, index) => (
+                    <SwiperSlide key={index}>
+                        <div className={"flex flex-col items-center"}>
+                            <img
+                                src={category.url}
+                                alt={category.name}
+                                className={"w-24 h-24 object-contain rounded-full p-2"}
+                            />
+                            <span className={"text-center text-lg font-medium mt-2"}>
+                                {category.name}
+                            </span>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    )
+}
+export default CategorySlide;
---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,39 @@
+﻿import './style.css'
+
+const Product = (props: any) => {
+    let brand: string = props.brand;
+    let title: string = props.title;
+    let discount: number = props.discount;
+    let price: number = props.price;
+    let newPrice: number = price * (1 - discount/100);
+    let image: string = props.image;
+
+    return (
+        <>
+        {/* Card sản phẩm */}
+            <div className="relative  p-4 pb-0 bg-white border border-gray-200 rounded-lg shadow-md">
+                {/* Ảnh */}
+                <img src={image} alt="Ảnh sản phẩm" className={"w-full h-40 object-contain mb-4"}/>
+                {/* Thương hiệu */}
+                <div className={"text-gray-500 text-sm uppercase mb-1"}>{brand}</div>
+                {/* Tên sản phẩm */}
+                <div className={"text-gray-800 text-base font-semibold mb-2 line-clamp-2"}>{title}</div>
+                {/* Giá */}
+                <div className={"grid items-center mb-4"}>
+                    <span className={"text-red-500 text-lg font-bold"}>{price.toLocaleString("vi-VN")}đ</span>
+                    <div className={"flex items-center space-x-2 "}>
+                        <span
+                            className={"text-gray-400 text-sm line-through"}>{newPrice.toLocaleString("vi-VN")}đ</span>
+                        {/* Badge discount */}
+                        <div
+                            className={" bg-red-400 text-white text-xs font-bold py-1 px-2 rounded-2xl"}>-{discount}%
+                        </div>
+                    </div>
+                </div>
+                <button
+                    className={"absolute right-4 bottom-9 w-8 h-8 bg-green-500 text-white rounded-full flex items-center justify-center text-lg font-bold"}>+</button>
+            </div>
+        </>
+    );
+};
+export default Product;
