9fba26254e3f9d3dc01fcf53892e5d1270a0f9cc
Admin <Admin@ADMIN-PC>
2025-01-23 18:16:29 +0700

    update map

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -23,6 +23,8 @@ const ContactPage = () => {
                 <li>Email: <a href="mailto:cskh@picare.vn">cskh@picare.vn</a></li>
                 <li>Website: <a href="http://shopduocmypham.com/">Picare Việt Nam</a></li>
             </ul>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2330.485428579429!2d106.66336862140763!3d10.781129530531969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ecd9b8e9949%3A0xfa29ef4fdfddb96d!2zUGlDYXJlIC0gU2hvcCBExrDhu6NjIE3hu7kgUGjhuqltIE5o4bqtcCBLaOG6qXUgVOG7qyBDaMOidSDDgnU!5e0!3m2!1svi!2s!4v1735893811845!5m2!1svi!2s"
+            className="w-full h-1/2 p-2"/>
             <Footer/>
         </Page>
     )
